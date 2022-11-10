0b11d90aad873f1b831682a76e5bd66de89190e9
Sergey Mount <i@b9sk.ru>
2022-11-10 19:16:08 +0300

    убрать html-теги из post[].content.rendered в ответе из fetch

--- news.js
+++ news.js
@@ -3,32 +3,39 @@ document.addEventListener("DOMContentLoaded", (e) => {
   renderNews(`offset=1&category__in[]=214&category__in[]=16829&posts_per_page=1`, 2)
 })
 
-function renderNews(queryString, position)
-{
+function renderNews(queryString, position) {
   fetch(`https://playmaker24.ru/wp-json/wp_query/args/?${queryString}`)
     .then(response => {
       return response.json()
     }).then(post => {
-      console.table(post)
+      // console.table(post)
       const link = post[0].link
       const date = post[0].date
       const title = post[0].title.rendered
-      const excerpt = post[0].excerpt.rendered
+      const excerpt = ((inputHTML) => {
+        const tempEl = document.createElement("div")
+        tempEl.innerHTML = inputHTML
+        return tempEl.textContent || tempEl.innerText || ""
+      })(post[0].content.rendered)
+      // console.log({excerpt});
 
       const elements = getTargetHTMLElements(position)
 
       elements.date.textContent = timeago.format(date, 'ru')
 
       elements.title.innerHTML = `<a href="${link}" target="_blank">${title}</a>`
-      elements.excerpt.innerHTML = excerpt
+      elements.excerpt.innerHTML = shorten(excerpt, 170).trim() + "..."
       elements.link.innerHTML = `<a href="${link}" target="_blank">Читать новость</a>`
 
     })
     .catch(console.error)
 }
 
-function getTargetHTMLElements(position)
-{
+function shorten(text, max) {
+  return text && text.length > max ? text.slice(0, max).split(' ').slice(0, -1).join(' ') : text
+}
+
+function getTargetHTMLElements(position) {
   if (!position) throw new Error("Агрумент position обязателен!")
   const date = document.querySelector(`.post${position}_date .tn-atom`)
   const title = document.querySelector(`.post${position}_title .tn-atom`)
