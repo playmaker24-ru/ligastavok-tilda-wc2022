2370b294a8c9d462952d730a265b3d0bc769fb6e
Sergey Mount <i@b9sk.ru>
2022-11-10 18:16:22 +0300

    добавить реальные данные, полученные через REST API

--- news.js
+++ news.js
@@ -1,13 +1,39 @@
-renderNews(`offset=0&category__in[]=214&category__in[]=16829&posts_per_page=1`)
-renderNews(`offset=1&category__in[]=214&category__in[]=16829&posts_per_page=1`)
+document.addEventListener("DOMContentLoaded", (e) => {
+  renderNews(`offset=0&category__in[]=214&category__in[]=16829&posts_per_page=1`, 1)
+  renderNews(`offset=1&category__in[]=214&category__in[]=16829&posts_per_page=1`, 2)
+})
 
-function renderNews(queryString, position = 1)
+function renderNews(queryString, position)
 {
   fetch(`https://playmaker24.ru/wp-json/wp_query/args/?${queryString}`)
     .then(response => {
       return response.json()
-    }).then(body => {
-      console.log(body);
+    }).then(post => {
+      console.table(post)
+      const link = post[0].link
+      const date = post[0].date
+      const title = post[0].title.rendered
+      const excerpt = post[0].excerpt.rendered
+
+      const elements = getTargetHTMLElements(position)
+
+      elements.date.textContent = timeago.format(date, 'ru')
+
+      elements.title.innerHTML = `<a href="${link}" target="_blank">${title}</a>`
+      elements.excerpt.innerHTML = excerpt
+      elements.link.innerHTML = `<a href="${link}" target="_blank">Читать новость</a>`
+
     })
     .catch(console.error)
+}
+
+function getTargetHTMLElements(position)
+{
+  if (!position) throw new Error("Агрумент position обязателен!")
+  const date = document.querySelector(`.post${position}_date .tn-atom`)
+  const title = document.querySelector(`.post${position}_title .tn-atom`)
+  const excerpt = document.querySelector(`.post${position}_excerpt .tn-atom`)
+  const link = document.querySelector(`.post${position}_link .tn-atom`)
+
+  return { link, date, title, excerpt }
 }
